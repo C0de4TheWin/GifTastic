3f09362f858d2edd9ee3e155e5018c02c00b8b41
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-03-26 22:35:32 -0700

    pushing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,17 +5,17 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     movies + "&api_key=Z2cyZ5palPTFMbQLQeF52yTe5T2FpnDi&limit=10";
 //this will assign data/attr to the buttons and bring in the info via Giphy API
 
-//create the on click function for when a button is pressed
-$("button").on("click", function() {
 
+//create the on click function for when a button is pressed
+//$("button").on("click", function() {
 
 function renderButtons() {
-     
+    $("#buttons").empty();
     var movies = $(this).attr("buttons");
     var data = $(this).attr("data-name");
     
     }
-})
+
 
 //Ajaxx will update part of the page without reloading
     
@@ -26,33 +26,23 @@ function renderButtons() {
         console.log(queryURL);
         console.log(response);
         
+        //create a loop so the code is executed repeatedly when a movie is searched it is added via append with its own button
+
         for (var i = 0; i < movies.length; i++) {
-            var a = $("<button>");
+            var a = $("<button>"); //creates button for each movie in array
             a.addClass("movie");
             a.attr("data-name", movies[i]);
             a.text(movies[i]);
             $("#buttons").append(a);
         }
-      })
+      })  //attempting to add buttons each search.
 
-      
-
-
-
-   //         var results = response.data;
-
-   //         for (var i = 0; i < results.length; i++) {
-   //             var gifDiv = $("<div class='item'>");
-
-   //            var rating = results[i].rating;
-
-  //              var p = $("<p>").text("Rating: " + rating);
-
-  //              var personImage = $("<img>");
-   //             personImage.attr("src", results[i].images.fixed_height.url);
-
-   //             gifDiv.prepend(p);
-   //             gifDiv.prepend(personImage);
-
-   //             $("#gifSpot").prepend(gifDiv);
+      //I think my issue might be having to recreate my button IDs in HTML
+      //button data-movies might not be right?
+      $("#buttons").on("click", function(event) {
+        event.preventDefault();
+        var movie = $("#movieAdd").val().trim();
+        movies.push(movie); });
+    
+        
     
